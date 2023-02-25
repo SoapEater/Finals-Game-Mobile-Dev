b6804fa844ec084336a4bce8b8d1db973d5718f6
SoapEater <116240879+SoapEater@users.noreply.github.com>
2023-02-25 15:53:00 +0800

    changes

--- Script/Character Script/Character.gd
+++ Script/Character Script/Character.gd
@@ -10,24 +10,30 @@ var velocity = Vector2()
 var on_ground = false
 #--------------------------------------------------------------------------------------------------------------#
 func _physics_process(delta):
-
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -SPEED
-	else:
-		velocity.x = 0
-	if Input.is_action_pressed("ui_jump"):
-		if on_ground == true:
-			velocity.y = JUMP_POWER
-			on_ground = false
-			
+	jump_grounded()
+	#--------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------#
+func jump_grounded():
+	#--------------------------------------------------#
+	if on_ground == true:
+		velocity.y = JUMP_POWER
+		on_ground = false	
 	velocity.y += GRAVITY
-	
 	if is_on_floor():
 		on_ground = true
 	else:
 		on_ground = false
-	
+	#--------------------------------------------------#
+func character_movement():
+	#--------------------------------------------------#
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_just_pressed("ui_jump"):
+		jump_grounded()
+	else:
+		velocity.x = 0
 	velocity = move_and_slide(velocity, FLOOR)
+	#--------------------------------------------------#
 #--------------------------------------------------------------------------------------------------------------#
