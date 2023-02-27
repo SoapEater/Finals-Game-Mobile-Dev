c0a79766cbba396e5b86eba37c3a34ac38871b42
SoapEater <116240879+SoapEater@users.noreply.github.com>
2023-02-27 18:12:33 +0800

    jumping mechanic implemented
    Proper jumping mechanic implemented using custom jump

--- Script/Character Script/Character Movement.gd
+++ Script/Character Script/Character Movement.gd
@@ -2,13 +2,13 @@
 extends KinematicBody2D
 #--------------------------------------------------------------------------------------------------------------#
 #variables
-export var speed : int
 var jumpPower : int
-var gravity : float
+var gravity : int
 #--------------------------------------------------------------------------------------------------------------#
 #variables for custom jump physics
-export var jumpPeak : float
-export var jumpHeight : int
+export var speed = 100
+export (float) var jumpPeak = .128
+export (int) var jumpHeight = 128
 #--------------------------------------------------------------------------------------------------------------#
 onready var animationPlayer = $Position2D/AnimationPlayer
 onready var position2D = $Position2D
@@ -21,16 +21,18 @@ func _ready():
 	jumpPower = gravity * jumpPeak
 #--------------------------------------------------------------------------------------------------------------#
 func _physics_process(delta):
-	gravity(delta)
+	#--------------------------------------------------#
+	gravity()
 	characterMovement(delta)
 	animationPlayer()
+	#--------------------------------------------------#
 #--------------------------------------------------------------------------------------------------------------#
 func characterMovement(delta):
 	#--------------------------------------------------#
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = - (speed * delta) * speed
+		velocity.x = -speed
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x = (speed * delta) * speed
+		velocity.x = speed
 	else:
 		velocity.x = 0
 	#--------------------------------------------------#
@@ -38,13 +40,13 @@ func characterMovement(delta):
 		velocity.x = 0
 	#--------------------------------------------------#	
 	if Input.is_action_pressed("ui_jump") && is_on_floor():
-		velocity.y = - (jumpPower * delta) * jumpPower
+		velocity.y = -jumpPower
 	#--------------------------------------------------#
 	velocity = move_and_slide(velocity, floors)
 	#--------------------------------------------------#
 #--------------------------------------------------------------------------------------------------------------#
-func gravity(delta):
-	velocity.y += gravity * delta
+func gravity():
+	velocity.y += gravity
 #--------------------------------------------------------------------------------------------------------------#
 func animationPlayer():
 	#--------------------------------------------------#
