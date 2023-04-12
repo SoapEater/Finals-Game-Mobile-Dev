3c7e9c798e23bb41259a797410463947506915d6
SoapEater <116240879+SoapEater@users.noreply.github.com>
2023-04-12 20:04:51 +0800

    Create Intro.gd
    Script for the intro screen

---
+++ Script/Miscellaneous Script/Intro/Intro.gd
@@ -0,0 +1,11 @@
+extends Control
+#--------------------------------------------------------------------------------------------------------------#
+onready var AnimationPlayerVar = get_node("AnimationPlayer")
+#--------------------------------------------------------------------------------------------------------------#
+func _ready():	
+	AnimationPlayerVar.play("Fade In")
+	yield(get_tree().create_timer(6), "timeout")
+	AnimationPlayerVar.play("Fade Out")
+	yield(get_tree().create_timer(3), "timeout")
+	get_tree().change_scene("res://Scene/Miscellaneous Scene/Menu Screen/Menu.tscn")
+#--------------------------------------------------------------------------------------------------------------#
